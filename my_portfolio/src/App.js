b8f68480e0318441f9784d1744580621506606e8
PrasadSatpute <prasadsatpute000@gmail.com>
2022-03-29 11:59:48 +0530

    Navbar v1

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import NAV from './Components/Nav/Nav'
 
 function App() {
   return (
     <div className="App">
-      
+      <NAV></NAV>
     </div>
   );
 }
--- my_portfolio/src/Components/Nav/Nav.js
+++ my_portfolio/src/Components/Nav/Nav.js
@@ -1,9 +1,22 @@
-function App() {
+import './NavStyle.css'
+import HomeICON from '../IMG/home.png'
+import ProfileICON from '../IMG/profile.png'
+import EducationICON from '../IMG/education.png'
+import ResumeICON from '../IMG/resume.png'
+import ContactICON from '../IMG/contact.png'
+
+function Navbar() {
   return (
     <div className="Navbar">
-      
+      <ul>
+          <li><a><img src={HomeICON}></img><h3>HOME</h3></a></li>
+          <li><a><img src={ProfileICON}></img><h3>ABOUT</h3></a></li>
+          <li><a><img src={EducationICON}></img><h3>EDUCATION</h3></a></li>
+          <li><a><img src={ResumeICON}></img><h3>RESUME</h3></a></li>
+          <li><a><img src={ContactICON}></img><h3>CONTACT</h3></a></li>
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default Navbar;
