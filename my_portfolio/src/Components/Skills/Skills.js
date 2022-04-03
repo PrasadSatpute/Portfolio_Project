de37fab8885c48151cc78af8771a6a968749e0f6
PrasadSatpute <prasadsatpute000@gmail.com>
2022-04-03 17:12:39 +0530

    Skills v1

--- my_portfolio/src/Components/Skills/Skills.js
+++ my_portfolio/src/Components/Skills/Skills.js
@@ -1,9 +1,200 @@
 import './Skills.css'
+import dot from '../IMG/dot.png'
+import dot1 from '../IMG/dot1.png'
 
 function Skills() {
   return (
     <div className="SkillsPage" id='Skills'>
-      <h1>Skills PAGE</h1>
+      <div className='ratingSkills'>
+        <div className='headingSkillsLeft'>
+          <h1><b>Skillset</b></h1>
+        </div>
+        <div className='skillsDetails'>
+          <div className='rowSkills'>
+            <div className='nameSkills'>HTML</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>CSS</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>JAVASCRIPT</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>C</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>C++</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>JAVA</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>ReactJS</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>MySql</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>Linux Commands</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>Node</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>Snowflake</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+          <div className='rowSkills'>
+            <div className='nameSkills'>Kafka</div>
+            <div className='levelSkills'>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+                    <img src={dot1}></img>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='toolsDetails'>
+        
+      </div>
     </div>
   );
 }
